3841ba98eedd5b918293636502c8cae9f1bc9269
bombcar <github@bombcar.com>
2023-11-22 12:36:37 -0600

    spotless

--- src/main/java/twilightforest/block/BlockTFNagastone.java
+++ src/main/java/twilightforest/block/BlockTFNagastone.java
@@ -589,16 +589,11 @@ public class BlockTFNagastone extends Block {
      */
     @Override
     public int damageDropped(int meta) {
-        if (meta > 3)
-        {
+        if (meta > 3) {
             return 1;
-        }
-        else
-        {
-            if (meta > 1)
-                return 0;
-            else
-                return meta;
+        } else {
+            if (meta > 1) return 0;
+            else return meta;
         }
     }
 

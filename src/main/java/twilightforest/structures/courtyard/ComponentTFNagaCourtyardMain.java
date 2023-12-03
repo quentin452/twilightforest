f3de67eea60cd0bd89ddcbebce88c52647b574d7
Gordon-Frohman <sanisimov6699@gmail.com>
2023-12-03 21:46:57 +0700

    Spotless applied (again)

--- src/main/java/twilightforest/structures/courtyard/ComponentTFNagaCourtyardMain.java
+++ src/main/java/twilightforest/structures/courtyard/ComponentTFNagaCourtyardMain.java
@@ -27,6 +27,7 @@ public class ComponentTFNagaCourtyardMain extends StructureTFComponent {
         EAST,
         WEST
     };
+
     int tableWidth = ROW_OF_CELLS * 2 - 1;
     int terraceLeft;
     CellType[][] courtyard = new CellType[tableWidth][tableWidth];
